312a191970e139fed485ae852aa67f25fb06ce41
jgomezz <jgomezz@gmail.com>
2024-04-07 12:31:08 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/tienda/entities/Categoria.java
@@ -0,0 +1,35 @@
+package pe.edu.tecsup.tienda.entities;
+
+public class Categoria {
+	
+	private Integer id;
+	private String nombre;
+	private Integer orden;
+	
+	// Setter’s and getter’s
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getOrden() {
+		return orden;
+	}
+	public void setOrden(Integer orden) {
+		this.orden = orden;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nombre=" + nombre + ", orden=" + orden + "]";
+	}
+	
+}
