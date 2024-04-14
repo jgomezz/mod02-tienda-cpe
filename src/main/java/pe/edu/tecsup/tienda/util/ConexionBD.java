1544b749ff8836a224a13d8cb51619800e4f973f
jgomezz <jgomezz@gmail.com>
2024-04-14 12:16:18 -0500

    Enable connection pool

--- src/main/java/pe/edu/tecsup/tienda/util/ConexionBD.java
+++ src/main/java/pe/edu/tecsup/tienda/util/ConexionBD.java
@@ -1,24 +1,52 @@
 package pe.edu.tecsup.tienda.util;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class ConexionBD {
-	
-	private static final String URL = "jdbc:mysql://localhost/tienda?useSSL=false";
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "";
-	
+		
 	public static Connection obtenerConexion() throws SQLException {
 		
 		Connection con = null;
+	
+		try {
+			
+			Context initContext = new InitialContext();
+			
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			
+			DataSource datasource = (DataSource) envContext.lookup("jdbc/tienda");
+			
+			con = datasource.getConnection();
+			
+		} catch (NamingException e) {
+			e.printStackTrace();	
+		} catch (Exception e) {
+			e.printStackTrace();
+		
+		}
+		
+		return con;
+	}
+
+	/*
+	public static Connection obtenerConexion() throws SQLException {
+		
+		Connection con = null;
+	
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		return con;
-	}
+	}*/
+	
 }
