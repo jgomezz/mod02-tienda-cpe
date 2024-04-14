cae222674aa1eaec7d7441a22d8eee125a8a4936
jgomezz <jgomezz@gmail.com>
2024-04-14 08:25:24 -0500

    Update ProductoRepository

--- src/main/java/pe/edu/tecsup/tienda/repositories/ProductoRepository.java
+++ src/main/java/pe/edu/tecsup/tienda/repositories/ProductoRepository.java
@@ -2,6 +2,11 @@ package pe.edu.tecsup.tienda.repositories;
 
 import org.apache.log4j.Logger;
 
+/**
+ * 
+ * @author jgomez
+ *
+ */
 public class ProductoRepository {
 	
 	private static final Logger log = Logger.getLogger(ProductoRepository.class);
